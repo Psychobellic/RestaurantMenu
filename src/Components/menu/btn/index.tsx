230b99bf918efd65e3142880c3c4c861309b3ca0
Matheus Schiffer <matheusschiffer@gmail.com>
2021-09-29 17:52:33 -0300

    moving

--- src/Components/menu/btn/index.tsx
+++ src/Components/menu/btn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 import styles from "./styles.module.scss";
@@ -9,6 +9,10 @@ export const Button = () => {
 	let [active, setActive] = useState(false);
 	let [selectedIndex, setSelectedIndex] = useState(0);
 
+	useEffect(() => {
+		setActive(active);
+	}, [active, selectedIndex]);
+
 	let Day = Object.values(WEEKDAYS).map((value, index) => {
 		let handleClick = () => {
 			setSelectedIndex(index);
@@ -17,35 +21,39 @@ export const Button = () => {
 
 		if (active && selectedIndex === index) {
 			return (
-				<div className={styles.btnWrap} key={index + value}>
-					<button
-						key={index + value}
-						type="button"
-						className={styles.btnActive}
-						onClick={handleClick}>
-						{value}
-					</button>
-					<span className={styles.active}>
-						<Image
-							src="/checkmark-png-25954.png"
-							width={64}
-							height={64}
-							alt="checkmark"
-						/>
-					</span>
-				</div>
+				<ul>
+					<li className={styles.btnWrap} key={index + value}>
+						<button
+							key={index + value}
+							type="button"
+							className={styles.btnActive}
+							onClick={handleClick}>
+							{value}
+						</button>
+						<span className={styles.active}>
+							<Image
+								src="/checkmark-png-25954.png"
+								width={64}
+								height={64}
+								alt="checkmark"
+							/>
+						</span>
+					</li>
+				</ul>
 			);
 		} else {
 			return (
-				<div className={styles.btnWrap} key={index}>
-					<button
-						type="button"
-						className={styles.btnInactive}
-						onClick={handleClick}>
-						{value}
-					</button>
-					<span className={styles.inactive}>{"?"}</span>
-				</div>
+				<ul>
+					<li className={styles.btnWrap} key={index}>
+						<button
+							type="button"
+							className={styles.btnInactive}
+							onClick={handleClick}>
+							{value}
+						</button>
+						<span className={styles.inactive}>{"?"}</span>
+					</li>
+				</ul>
 			);
 		}
 	});
