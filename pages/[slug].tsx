c7c931b6455807b5bca54028b8352c2c0a7c1792
Matheus Schiffer <matheusschiffer@gmail.com>
2021-10-03 14:52:47 -0300

    final

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -25,7 +25,7 @@ export const Slug = () => {
 						<br />
 						<br />
 						Para <strong>acompanhamento</strong> selecione
-						<strong>uma</strong>
+						<strong>uma </strong>
 						das opções abaixo:
 					</span>
 					<div className={styles.cardContainer}>
@@ -61,54 +61,7 @@ export const Slug = () => {
 			</>
 		);
 	} else if (process.browser && text === "/1") {
-		return (
-			<>
-				<div className={styles.day}>
-					<Link href={"/0"} className={styles.dayTitle}>
-						<h1>SEGUNDA 01/01</h1>
-						<ArrowForward />
-					</Link>
-				</div>
-				<div className={styles.day}>
-					<Link href={"/1"} className={styles.dayTitle}>
-						<h1>TERÇA 02/01</h1>
-						<ArrowDown />
-					</Link>
-					<span className={styles.dayContent}>
-						<strong>Guarnição:</strong> arroz, feijão, abobrinha, alface, rúcula
-						<br />
-						<strong>Sobremesa:</strong>arroz doce
-						<br />
-						<br />
-						Para <strong>acompanhamento</strong> selecione <strong>uma</strong>
-						das opções abaixo:
-					</span>
-					<div className={styles.cardContainer}>
-						<Card option="OPÇÃO 1" content="- Bisteca de Porco" />
-						<Card option="OPÇÃO 2" content="- Omelete" />
-						<Card option="OPÇÃO 3" content="- Filé de Frango" />
-					</div>
-				</div>
-				<div className={styles.day}>
-					<Link href={"/2"} className={styles.dayTitle}>
-						<h1>QUARTA 03/01</h1>
-						<ArrowForward />
-					</Link>
-				</div>
-				<div className={styles.day}>
-					<Link href={"/3"} className={styles.dayTitle}>
-						<h1>QUINTA 04/01</h1>
-						<ArrowForward />
-					</Link>
-				</div>
-				<div className={styles.day}>
-					<Link href={"/4"} className={styles.dayTitle}>
-						<h1>SEXTA 06/01</h1>
-						<ArrowForward />
-					</Link>
-				</div>
-			</>
-		);
+		return <RestaurantMenu />;
 	} else if (process.browser && text === "/2") {
 		return (
 			<>
